2dd6ba92015129bb25664b8ee8c1f43132a298ab
Megan Horsfield <meganthadev@gmail.com>
2020-03-06 04:26:44 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,12 +1,12 @@
 require 'nokogiri'
 require 'pry'
 
-def create_project_hash
-  html = File.read('fixtures/kickstarter.html')
-end
+  def create_project_hash
+    html = File.read('fixtures/kickstarter.html')
+  end
 
-kickstarter = Nokogiri::HTML(html)
-binding.pry
+  kickstarter = Nokogiri::HTML(html)
+  binding.pry
 end 
 
 create_project_hash
