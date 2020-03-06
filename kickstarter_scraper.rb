72766f4fecab1fc79d46c7c2368cb146566f1a80
Megan Horsfield <meganthadev@gmail.com>
2020-03-06 04:59:22 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -12,7 +12,12 @@ end
   kickstarter.css("li.project.grid_4").each do |project|
     title = 
     project.css("h2.bb card_name strong a").text 
-      projects[title.to_sym] = {}
+      projects[title.to_sym] = {:image_link => project.css("div.project-thumbnail a img").attribute("src").value,
+      :description => project.css("p.bbcard_blurb").text,
+      :location => project.css("ul.project-meta span.location-name").text,
+      :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
+      }
+  end
     end 
     projects
   end
