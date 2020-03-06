8686f11c59d4f88047ec3a603c79550f3f12940d
Megan Horsfield <meganthadev@gmail.com>
2020-03-06 04:59:37 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -17,7 +17,6 @@ end
       :location => project.css("ul.project-meta span.location-name").text,
       :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
       }
-  end
     end 
     projects
   end
